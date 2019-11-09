8b223ac66400d00d45e0982e26da062605b06635
colw <colw@outlook.com>
2019-11-09 17:11:59 +0100

    Add page refresh to html

--- functions/sugar/sugar.js
+++ functions/sugar/sugar.js
@@ -1,6 +1,43 @@
 /* eslint-disable */
 // for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
 
+function createPage(value) {
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <body>
+    <div>${value.sgv} ${getArrowChar(value.direction)}</div>
+    <script>
+      setTimeout(() => window.location.reload(), 30 * 1000)
+    </script>
+    </body>
+    </html>
+  `;
+  return html;
+}
+
+const fetch = require("node-fetch");
+exports.handler = async function(event, context) {
+  try {
+    const response = await fetch(process.env.ENTRIES_URL);
+    if (response.status != 200) {
+      return { statusCode: response.status, body: { error: statusText } };
+    }
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: createPage(data[0])
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+    };
+  }
+};
+
 function getArrowChar(direction) {
   let arrow;
   switch (direction) {
@@ -42,37 +79,3 @@ function getArrowChar(direction) {
   }
   return arrow;
 }
-
-function createPage(value) {
-  const html = `
-    <!DOCTYPE html>
-    <html>
-    <body>
-    <h1>${value.sgv} ${getArrowChar(value.direction)}</h1>
-    </body>
-    </html>
-  `;
-  return html;
-}
-
-const fetch = require("node-fetch");
-exports.handler = async function(event, context) {
-  try {
-    const response = await fetch(process.env.ENTRIES_URL);
-    if (response.status != 200) {
-      return { statusCode: response.status, body: { error: statusText } };
-    }
-    const data = await response.json();
-
-    return {
-      statusCode: 200,
-      body: createPage(data[0])
-    };
-  } catch (err) {
-    console.log(err); // output to netlify function log
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
-    };
-  }
-};
