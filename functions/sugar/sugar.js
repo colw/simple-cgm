24d59ebdbb1add87c021069e0d8f3c9b75f7a667
colw <colw@outlook.com>
2019-11-09 16:57:33 +0100

    Display direction arrow

--- functions/sugar/sugar.js
+++ functions/sugar/sugar.js
@@ -1,12 +1,54 @@
 /* eslint-disable */
 // for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
 
+function getArrowChar(direction) {
+  let arrow;
+  switch (direction) {
+    case "TripleUp":
+      arrow = "⤊";
+      break;
+    case "DoubleUp":
+      arrow = "⇈";
+      break;
+    case "SingleUp":
+      arrow = "↑";
+      break;
+    case "FortyFiveUp":
+      arrow = "↗";
+      break;
+    case "Flat":
+      arrow = "→";
+      break;
+    case "FortyFiveDown":
+      arrow = "↘";
+      break;
+    case "SingleDown":
+      arrow = "↓";
+      break;
+    case "DoubleDown":
+      arrow = "⇊";
+      break;
+    case "TripleDown":
+      arrow = "⤋";
+      break;
+    case "NOT COMPUTABLE":
+      arrow = "-";
+      break;
+    case "RATE OUT OF RANGE":
+      arrow = "⇕";
+      break;
+    default:
+      arrow = "~";
+  }
+  return arrow;
+}
+
 function createPage(value) {
   const html = `
     <!DOCTYPE html>
     <html>
     <body>
-    <h1>${value.sgv}</h1>
+    <h1>${value.sgv} ${getArrowChar(value.direction)}</h1>
     </body>
     </html>
   `;
